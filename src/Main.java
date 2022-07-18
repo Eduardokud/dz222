7f51ada687155063731a9efdc6b5d9c5d57320a3
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-19 01:45:20 +0300

    reformat commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int c = 200; //балланс клиента
         int p = 700;//сумма пополнения
         int b;//начисленные бонусы
         if (p >= 1000) {
             b = p / 100;
-        }
-        else {
+        } else {
             b = 0;
         }
-int i = c + p + b;
+        int i = c + p + b;
         System.out.println("бонус равен " + b + " рублям, " + "итоговая сумма на счету клиента " + i + " рублей");
     }
 
