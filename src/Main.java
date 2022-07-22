21083d0f72b16afe5050234f254ec8929c9e31c0
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-22 07:11:21 +0300

    change names

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int c = 200; //балланс клиента
-        int p = 700;//сумма пополнения
-        int b;//начисленные бонусы
-        if (p >= 1000) {
-            b = p / 100;
+        int ballans = 200; //балланс клиента
+        int popolnenie = 700;//сумма пополнения
+        int bonus;//начисленные бонусы
+        if (popolnenie >= 1000) {
+            bonus = popolnenie / 100;
         } else {
-            b = 0;
+            bonus = 0;
         }
-        int i = c + p + b;
-        System.out.println("бонус равен " + b + " рублям, " + "итоговая сумма на счету клиента " + i + " рублей");
+        int i = ballans + popolnenie + bonus;
+        System.out.println("бонус равен " + bonus + " рублям, " + "итоговая сумма на счету клиента " + i + " рублей");
     }
 
 }
